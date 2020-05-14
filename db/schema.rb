af210d281e992ae8ba4ab312442a38155e30ec6e
Expedito Andrade <expeditojazz@gmail.com>
2020-05-14 16:08:33 -0300

    Added stock model and iex api key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_130145) do
+ActiveRecord::Schema.define(version: 2020_05_14_183932) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
