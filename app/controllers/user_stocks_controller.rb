0d4e28d97498e8b5688d2fa6331b57409158284e
Expedito Andrade <expeditojazz@gmail.com>
2020-05-17 09:28:45 -0300

    Added UI for the stock user association

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
 class UserStocksController < ApplicationController
+  def create
+    stock = Stock.check_db(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker])
+      stock.save
+    end
+    if current_user.stocks.include?(stock) == false
+      @user_stock = UserStock.create(user: current_user, stock: stock)
+      flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+    else
+      flash[:notice] = 'You already have this stock on your portfolio'
+    end
+
+    redirect_to my_portfolio_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def can_track_stock?(ticker_symbol)
+    true if under_stock_limit? && stock_already_tracked?(ticker_symbol) == false
+  end
+
+  def under_stock_limit?
+    current_user.stocks.count < 5
+  end
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    false unless current_user.stocks.include?(stock)
+  end
 end
